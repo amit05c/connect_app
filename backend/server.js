82aadae6789544dc98137ba48ab7f73f66c42d8f
amig05c <amitghosh0595c@gmail.com>
2022-12-08 15:19:43 +0530

    delete previous repo

---
+++ backend/server.js
@@ -0,0 +1,70 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const {connection}= require("./config/db")
+const userRoutes= require("./routes/userRouters")
+const chatRoutes = require("./routes/chatRouter");
+const messageRoutes = require("./routes/messageRoutes")
+const cors= require("cors")
+
+const app = express();
+app.use(cors())
+app.use(express.json());
+
+app.use("/api/user", userRoutes);
+app.use("/api/chat", chatRoutes);
+app.use("/api/message", messageRoutes);
+
+const PORT = process.env.PORT || 8080
+
+const server=app.listen(PORT,async()=>{
+    try{
+        await connection
+        console.log("connected")
+    }catch(err){
+        console.log(err.message)
+    }
+})
+
+
+// socket io
+
+const io = require("socket.io")(server, {
+    pingTimeout: 60000,
+    cors: {
+      origin: "http://localhost:3000",
+      // credentials: true,
+    },
+  });
+
+  io.on("connection", (socket) => {
+    console.log("Connected to socket.io");
+    socket.on("setup", (userData) => {
+      socket.join(userData._id);
+      socket.emit("connected");
+    });
+  
+    socket.on("join chat", (room) => {
+      socket.join(room);
+      console.log("User Joined Room: " + room);
+    });
+    socket.on("typing", (room) => socket.in(room).emit("typing"));
+    socket.on("stop typing", (room) => socket.in(room).emit("stop typing"));
+  
+    socket.on("new message", (newMessageRecieved) => {
+      var chat = newMessageRecieved.chat;
+  
+      if (!chat.users) return console.log("chat.users not defined");
+  
+      chat.users.forEach((user) => {
+        if (user._id == newMessageRecieved.sender._id) return;
+  
+        socket.in(user._id).emit("message recieved", newMessageRecieved);
+      });
+    });
+  
+    socket.off("setup", () => {
+      console.log("USER DISCONNECTED");
+      socket.leave(userData._id);
+    });
+  });
+  
