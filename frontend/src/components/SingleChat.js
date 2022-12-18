f3abc889b26e26c37c7432292e935981cb9674f3
amig05c <amitghosh0595c@gmail.com>
2022-12-18 19:20:56 +0530

    test

--- frontend/src/components/SingleChat.js
+++ frontend/src/components/SingleChat.js
@@ -16,7 +16,7 @@ import io from "socket.io-client";
 import UpdateGroupChatModal from "./miscellaneous/UpdateGroupChatModal";
 import { ChatContext } from "../Context/ChatProvider";
 
-const ENDPOINT = "https://connectapp-production.up.railway.app"
+const ENDPOINT = "https://woozy-bottle-production.up.railway.app"
 //"http://localhost:8080"
 //"https://chat-app-production-6faf.up.railway.app"; // "; -> After deployment
 var socket, selectedChatCompare;
